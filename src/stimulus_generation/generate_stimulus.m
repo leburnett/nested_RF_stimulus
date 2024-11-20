fde3585a1ccb8824d2435ab8f70b44870d88e702
leburnett <leburnett3@gmail.com>
2024-11-20 15:37:45 -0500

    Update after testing on rig

--- src/stimulus_generation/generate_stimulus.m
+++ src/stimulus_generation/generate_stimulus.m
@@ -1,6 +1,6 @@
 
 %% Generate nested flash protocol for determining the receptive field of T4 / T5 cells
-ROOT_DIR = '/Users/burnettl/Documents/GitHub/nested_RF_stimulus/';
+ROOT_DIR = 'C:\matlabroot\nested_RF_stimulus';
 
 % Arena parameters - [n_rows, n_cols, n_pix_per_panel]
 arena_size = [3, 12, 16];
@@ -22,13 +22,13 @@ flash_sz_px = 12;
 patName = strcat(string(flash_sz_px),'px_square_RF_ON_OFF_', string(px_rng_to_use(3)), '_', string(px_rng_to_use(4)));
 
 % Directory to save pattern:
-patt_save_dir = fullfile(ROOT_DIR, 'results/patterns');
+patt_save_dir = fullfile(ROOT_DIR, 'results\patterns');
 if ~isfolder(patt_save_dir)
     mkdir(patt_save_dir);
 end 
 
 % Generate and save flash pattern:
-generate_flash_pattern(arena_size, px_intensity, px_rng_to_use, flash_sz_px, patName, patt_save_dir)
+n_frames = generate_flash_pattern(arena_size, px_intensity, px_rng_to_use, flash_sz_px, patName, patt_save_dir);
 
 % _________________________________________________________________________
 
@@ -49,10 +49,10 @@ func = generate_func_for_flash(bkg_frame, bkg_dur, flash_seq, flash_dur);
 n_frames_func = numel(func);
 disp(strcat("Number of elements in 'func': ", string(n_frames_func)))
 
-n_unique_frame_pos = unique(func);
+n_unique_frame_pos = numel(unique(func));
 disp(strcat("Number of unique frames in 'func': ", string(n_unique_frame_pos)))
 
-func_save_dir = fullfile(ROOT_DIR, 'results/functions');
+func_save_dir = fullfile(ROOT_DIR, 'results\functions');
 if ~isfolder(func_save_dir)
     mkdir(func_save_dir);
 end 
@@ -62,11 +62,13 @@ param.func = func;
 function_type = 'pfn';
 ID = get_function_ID(function_type, func_save_dir);
 param.ID = ID;
+param.frames = 65; % n_frames; % number of frames in the pattern.
+param.gs_val = 4;
 param.type = function_type;
 % String to use for function name.
 filename = strcat(string(flash_sz_px), 'px_flashes_', string(px_rng_to_use(3)), '_', string(px_rng_to_use(4)), '_', string(n_frames_func), 'frames');
 
-save_function_G4(func, param, save_dir, filename);
+save_function_G4(func, param, func_save_dir, filename);
 
 % _________________________________________________________________________
 
