6ef8c7e7b7564971ec9ecab4c7455323a435275b
leburnett <leburnett3@gmail.com>
2025-01-29 10:44:50 -0500

    Update combine_polar_plots.m
    Generate version of linear timeseries plot with the initial value subtracted from the voltage so that they all start at 0mV.

--- src/analysis/protocol2/combine_polar_plots.m
+++ src/analysis/protocol2/combine_polar_plots.m
@@ -233,6 +233,28 @@ title('RNAi ttl - OFF')
 
 
 
+%%
+
+figure
+offset = 0;
+celll = 7;
+for pp = 1:16
+
+    da = data_aligned{pp, celll}*10;
+    plot(da-offset, 'LineWidth', 0.2);
+    hold on
+    offset = offset+5;
+end 
+box off
+title(string(celll))
+
+f = gcf;
+f.Position = [1   543   254   504];
+
+
+
+
+
 %% FInd the mean across all of the cells - plot the timeseries - PEAKS ARE NOT ALIGNED. 
 
 f3 = figure;
@@ -375,6 +397,97 @@ end
 
 f = gcf;
 f.Position = [18  688  1762 316];
+
+
+%% With the peaks of the data aligned AND the initial voltage subtracted so that they all start at 0mV
+
+% "/Users/burnettl/Documents/Projects/nested_RF_stimulus/protocol2/results/OFF_all"
+
+figure
+
+for d = 1:32
+    
+    subplot(2, 16, d)
+
+    arrays = data_aligned(d, :);
+    num_arrays = length(arrays);
+    
+    % Find peak indices
+    peak_indices = zeros(1, num_arrays);
+    array_lengths = zeros(1, num_arrays);
+    
+    for q = 1:num_arrays
+        [~, peak_indices(q)] = max(arrays{q});
+        array_lengths(q) = length(arrays{q});
+    end
+    
+    % Determine the maximum peak index
+    max_peak = max(peak_indices);
+    
+    % Compute shifts needed
+    shifts = max_peak - peak_indices;
+    
+    % Determine the new length (maximum shifted length)
+    new_length = max(array_lengths + shifts);
+    
+    % Create shifted arrays
+    aligned_arrays = nan(num_arrays, new_length);  % Use NaN padding
+    
+    for i2 = 1:num_arrays
+        if i2<=5
+            col = [0.8 0.8 0.8];
+        else 
+            col = [1 0.8 0.8];
+        end 
+
+        ddd = arrays{i2}; % data 
+        x = [1:1:numel(ddd)]+shifts(i2);
+        xq = 1:1:new_length;
+        vq = interp1(x, ddd, xq, 'nearest', 'extrap'); % extrapolate data 
+        vq = vq-vq(1);
+        aligned_arrays(i2, :) = vq;
+        plot(aligned_arrays(i2, :)*10, 'Color', col)
+        hold on
+    end
+
+    plot(nanmean(aligned_arrays(1:5, :))*10, 'Color', 'k', 'LineWidth', 1.5)
+    plot(nanmean(aligned_arrays(6:end, :))*10, 'Color', 'r', 'LineWidth', 1.5)
+
+    box off
+    ax = gca;
+    ax.TickLength = [0.04 0.04];
+    ax.TickDir = 'out';
+    xlim([0 new_length])
+
+    if d>16
+        % ylim([-73 -45])
+        ax.XTickLabel = {'0', '0.5', '1'};
+    else
+        % ylim([-73 -35])
+        ax.XTickLabel = {'0', '1', '2'};
+    end 
+
+    if d ==1 || d == 17
+        ylabel('Voltage (mV)');
+        xlabel('Time (s)')
+    else
+        ax.YAxis.Visible = 'off';
+    end 
+
+    if d == 8 
+        title('28 dps')
+    elseif d == 24
+        title('56 dps')
+    end 
+
+    ylim([-10 35])
+end 
+
+f = gcf;
+f.Position = [18  688  1762 316];
+
+
+
 
 %% Median voltage per recording
 
@@ -704,6 +817,13 @@ end
 
 
 %% 
+
+
+
+
+
+
+
 
 
 
