8afb99dbdda365eea262cb9f4e0f33b8ccaec228
leburnett <leburnett3@gmail.com>
2025-01-30 10:41:30 -0500

    Update plot_timeseries_init0_alignedpeaks.m
    Generated a function from this code

--- src/analysis/plotting/plot_timeseries_init0_alignedpeaks.m
+++ src/analysis/plotting/plot_timeseries_init0_alignedpeaks.m
@@ -8,32 +8,8 @@ function plot_timeseries_init0_alignedpeaks(data_folder)
 % that they all start at 0mV.
 
 % data_folder = "/Users/burnettl/Documents/Projects/nested_RF_stimulus/protocol2/results/OFF_all"
-cd(data_folder)
-file_list = dir('*peak_vals*');
-n_exp = height(file_list);
 
-% Initialise empty arrays for filling in:
-data_align_all = cell(32, n_exp);
-ctrl_flies = zeros(n_exp, 1);
-
-%% Combine the data from all of the cells of this type:
-
-for i = 1:n_exp
-
-    % Load needed variables:
-    load(file_list(i).name);
-
-    % Determine if the data is from a control or test fly.
-    if contains(file_list(i).name, 'CTL')
-        ctrl_flies(i) = 1;
-    end 
-
-    % Combine the "mean" aligned timeseries from all of the flies.  
-    data_align_all(:, i) = data_aligned(:, 4); % Combine the mean timeseries only.
-end 
-
-% Convert 'ctrl_flies' from double to logical for logical indexing. 
-ctrl_flies = logical(ctrl_flies);
+[data_align_all, ctrl_flies] = combine_data_find_ctrls(data_folder);
 
 %% Plot both control and turtle flies together. 
 % Individual subplots for each direction. 
