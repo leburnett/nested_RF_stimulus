f340cb5f49b3c254f90c281a9e7e1656ad469c13
leburnett <leburnett3@gmail.com>
2025-04-08 12:28:48 -0400

    Add save_fig and project_root to plot functions

--- src/analysis/protocol2/pipeline/quality_check_plots/plot_quality_check_data_full_rec.m
+++ src/analysis/protocol2/pipeline/quality_check_plots/plot_quality_check_data_full_rec.m
@@ -1,4 +1,4 @@
-function plot_quality_check_data_full_rec(f_data, v_data)
+function plot_quality_check_data_full_rec(f_data, v_data, save_fig, PROJECT_ROOT)
 
 figure; 
 
@@ -27,6 +27,16 @@ plot(movmean(v_data, 100000))
 title(strcat("var - movmean - 100000 = ", string(var(movmean(v_data, 100000)))))
 f = gcf;
 f.Position = [25 629 1121 392];
+
+if save_fig
+    figures_folder = fullfile(PROJECT_ROOT, "figures", "quality");
+    fname = fullfile(figures_folder, strcat('Quality_full_rec_', date_str, '_', time_str, '_', strain_str, '_', type_str,  ".pdf"));
+    exportgraphics(f ...
+        , fname ...
+        , 'ContentType', 'vector' ...
+        , 'BackgroundColor', 'none' ...
+        ); 
+end 
 
 end 
 
--- src/analysis/protocol2/pipeline/quality_check_plots/plot_quality_check_var_reps_prctile.m
+++ src/analysis/protocol2/pipeline/quality_check_plots/plot_quality_check_var_reps_prctile.m
@@ -1,4 +1,4 @@
-function plot_quality_check_var_reps_prctile(var_across_reps, var_within_reps, diff_mean, max_data, min_data)
+function plot_quality_check_var_reps_prctile(var_across_reps, var_within_reps, diff_mean, max_data, min_data, save_fig, PROJECT_ROOT)
 
     % With median value across all flashes in title.
     figure; 
@@ -28,11 +28,22 @@ function plot_quality_check_var_reps_prctile(var_across_reps, var_within_reps, d
     subplot(2,3,6)
     histogram(diff_mean);
     xlabel('Diff between max and min of mean')
-    
+     
     f = gcf;
     f.Position = [1   684   827   363];
-    % savefig(gcf, fullfile(qual_fig_folder, strcat('Var_', date_str, '_', strain_str, '_', type_str)))
-    
+
+    if save_fig
+        figures_folder = fullfile(PROJECT_ROOT, "figures", "quality");
+        fname = fullfile(figures_folder, strcat('Quality_var_', date_str, '_', time_str, '_', strain_str, '_', type_str,  ".pdf"));
+        exportgraphics(f ...
+            , fname ...
+            , 'ContentType', 'vector' ...
+            , 'BackgroundColor', 'none' ...
+            ); 
+    end 
+
+    % % % 
+
     figure; 
     subplot(2,1,1)
     imagesc(max_data); colorbar; title('max - 98th prctile')
@@ -40,7 +51,18 @@ function plot_quality_check_var_reps_prctile(var_across_reps, var_within_reps, d
     histogram(max_data)
     f=gcf;
     f.Position = [620   501   275   466];
-    % savefig(gcf, fullfile(qual_fig_folder, strcat('Max_', date_str, '_', strain_str, '_', type_str)))
+
+    if save_fig
+        figures_folder = fullfile(PROJECT_ROOT, "figures", "quality");
+        fname = fullfile(figures_folder, strcat('Quality_max_heatmap', date_str, '_', time_str, '_', strain_str, '_', type_str,  ".pdf"));
+        exportgraphics(f ...
+            , fname ...
+            , 'ContentType', 'vector' ...
+            , 'BackgroundColor', 'none' ...
+            ); 
+    end 
+
+    % % %
     
     figure; 
     subplot(2,1,1)
@@ -49,7 +71,15 @@ function plot_quality_check_var_reps_prctile(var_across_reps, var_within_reps, d
     histogram(min_data)
     f=gcf;
     f.Position = [620   501   275   466];
-    % savefig(gcf, fullfile(qual_fig_folder, strcat('Min_', date_str, '_', strain_str, '_', type_str)))
 
+    if save_fig
+        figures_folder = fullfile(PROJECT_ROOT, "figures", "quality");
+        fname = fullfile(figures_folder, strcat('Quality_min_heatmap', date_str, '_', time_str, '_', strain_str, '_', type_str,  ".pdf"));
+        exportgraphics(f ...
+            , fname ...
+            , 'ContentType', 'vector' ...
+            , 'BackgroundColor', 'none' ...
+            ); 
+    end 
 
 end 
