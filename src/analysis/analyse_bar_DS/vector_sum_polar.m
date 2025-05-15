44479e8190e300fadff5c8a672615ac154941f34
leburnett <leburnett3@gmail.com>
2025-05-15 10:42:34 -0400

    Update vector_sum_polar.m
    Tried this different method of finding the resultant angle - the result looks the same for our best T4 cell

--- src/analysis/analyse_bar_DS/vector_sum_polar.m
+++ src/analysis/analyse_bar_DS/vector_sum_polar.m
@@ -9,13 +9,19 @@ function [resultant_magnitude, resultant_angle] = vector_sum_polar(rho, theta)
     end
 
     % Convert polar coordinates to Cartesian coordinates
-    [x, y] = pol2cart(theta, rho);
-    
-    % Compute the vector sum in Cartesian coordinates
-    sum_x = sum(x);
-    sum_y = sum(y);
-    
-    % Convert back to polar coordinates
-    [resultant_angle, resultant_magnitude] = cart2pol(sum_x, sum_y); % cart2pol returns angle first
+    % [x, y] = pol2cart(theta, rho);
+    x = sum(rho .* cos(theta));
+    y = sum(rho .* sin(theta));
+
+    % % Compute the vector sum in Cartesian coordinates
+    % sum_x = sum(x);
+    % sum_y = sum(y);
+    % 
+    % % Convert back to polar coordinates
+    % [resultant_angle, resultant_magnitude] = cart2pol(sum_x, sum_y); % cart2pol returns angle first
+
+    resultant_angle = atan2(y, x);
+    resultant_angle = mod(resultant_angle, 2*pi); % ensure it's in [0, 2pi]
+    resultant_magnitude = sqrt(x^2 + y^2);
 
 end
