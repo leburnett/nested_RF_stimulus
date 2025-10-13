f75dc14ca8329ed93fa5f10428d4ad8f12f10135
leburnett <leburnett3@gmail.com>
2025-10-13 17:29:01 -0400

    Create generate_bar_flash_stimulus_xy.m

---
+++ src/stimulus_generation/pattern/generate_bar_flash_stimulus_xy.m
@@ -0,0 +1,99 @@
+function generate_bar_flash_stimulus_xy(x, y, px_intensity, px_crop, on_off, exp_folder, n_flank)
+% Generate inset bar patterns centred on [X, Y]
+    
+% From an array of the "central" frame for each pattern. Choose "n_flank"
+% frames either side of the central frame. 
+% If "n_flank" = 5, then there will be 11 frames per bar orientation. 
+% There are 8 orientations in total so if "n_flank" = 5, then 11 x 8 = 88
+% frames.
+
+% These will then be randomly selected. 
+
+    n_orientations = 8;
+
+    centre_frames = [30, 33, 33, 34, 34, 34, 34, 35, 34, 34, 32, 32, 32, 34, 34, 34]; % check what the centre frames of the dark bars are.... JY comp. 
+
+    save_dir = strcat(exp_folder, '\patterns');
+    if ~isfolder(save_dir)
+        mkdir(save_dir);
+    end
+    
+    % Path to the folder with the full field bar stimuli.
+    full_bar_patterns_dir = 'C:\matlabroot\G4_Protocols\nested_RF_stimulus\results\patterns\protocol2\full_field_bars4'; % July 2025 - changed to 4bkg
+    cd(full_bar_patterns_dir)
+    
+    bar_patts = dir('*.mat');
+    
+    if on_off == "on"
+        patt_ids = 1:1:8;
+    elseif on_off == "off"
+        patt_ids = 9:1:16;
+    elseif on_off == "both"
+        patt_ids = 1:1:16;
+    end 
+
+    % Make empty pattern - this will include the frames for each
+    % orientation.
+    n_frames_orient = (n_flank*2 + 1);
+    n_frames_pattern = n_frames_orient * n_orientations;
+    bkg_color = px_intensity(1);
+    arena_px_h = 48;
+    arena_px_w = 192;
+    
+    % Make empty pattern array purely with grey background.
+    Pats = ones(arena_px_h, arena_px_w, n_frames_pattern)*bkg_color;
+
+    frames_filled = 0;
+    
+    % Generate 'cropped' bar stimuli centred on the [x,y] coordinates.
+    for p = patt_ids
+
+        fname = bar_patts(p).name;
+        patt_file = fullfile(bar_patts(p).folder, fname);
+
+        [disp_y1, disp_y2, disp_x1, disp_x2] = centeredSquare(x, y, px_crop);
+
+        % crop around the centre of the arena screen [24, 96];
+        arena_px_centre_h = arena_px_h/2;
+        arena_px_centre_w = arena_px_w/2;
+        px_side = (px_crop-1)/2;
+        crop_h_st = int8(arena_px_centre_h-px_side); 
+        crop_h_end = int8(arena_px_centre_h+px_side);
+        crop_w_st = int8(arena_px_centre_w-px_side);
+        crop_w_end = int8(arena_px_centre_w+px_side);
+        px_rng = [crop_h_st, crop_h_end, crop_w_st, crop_w_end];
+        
+        overall_frame_range = (1:n_frames_orient)+frames_filled;
+
+        cf_of_pattern = centre_frames(p); % Which frame number has the bar in the centre of the arena. 
+        frames_from_patt = cf_of_pattern-n_flank : cf_of_pattern+n_flank;
+
+        % Fill in the location with the central part of pattern. 
+        for i = 1:n_frames_orient
+
+            % Index in which to find the frame
+            frame_id = frames_from_patt(i);
+
+            % index in which to store the frame
+            overall_id = overall_frame_range(i);
+
+            Pats(int16(disp_y1):int16(disp_y2), int16(disp_x1):int16(disp_x2), overall_id) = pattern.Pats(crop_h_st:crop_h_end, crop_w_st:crop_w_end, frame_id);
+        end
+
+        frames_filled = frames_filled + (n_frames_orient*p);
+
+    end 
+
+    patName = strcat(fname(6:end-14), 'CROP', string(px_crop), '_X', string(x), '_Y', string(y), '_FLASHES');
+    param = pattern.param;
+    param.stretch = zeros(size(Pats, 3), 1);
+    param.gs_val = 4;
+    param.arena_pitch = 0;
+    param.px_rng = px_rng;
+    
+    param.ID = get_pattern_ID(save_dir);
+        
+    save_pattern_G4(Pats, param, save_dir, patName);
+
+ 
+end 
