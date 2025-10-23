532f6ac52acdd38d326482956e28bfcab4453870
leburnett <leburnett3@gmail.com>
2025-10-23 16:51:00 -0400

    update

--- src/analysis/process_protocol2.m
+++ src/analysis/process_protocol2.m
@@ -4,7 +4,7 @@ function process_protocol2()
 
 exp_folder = cd;
 
-PROJECT_ROOT = "/Users/burnettl/Documents/Projects/nested_RF_stimulus/protocol2";
+PROJECT_ROOT = "C:\matlabroot\G4_Protocols\nested_RF_protocol2";
 
 % Load metadata - contains 'Frame', 'Age', 'Strain' and 'Side'. Side is the
 % side of the arena upon which protocol 1 was run. 
