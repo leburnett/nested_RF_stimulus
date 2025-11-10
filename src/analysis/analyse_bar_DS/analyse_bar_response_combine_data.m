a4515a495378816ac0a1b60de8f90c1f904a11a4
leburnett <leburnett3@gmail.com>
2025-11-10 10:44:56 -0500

    Add functions for box whisk plot and stats from T

--- src/analysis/analyse_bar_DS/analyse_bar_response_combine_data.m
+++ src/analysis/analyse_bar_DS/analyse_bar_response_combine_data.m
@@ -1,32 +1,55 @@
 
 % Assess bar responses across the different cohorts - split into ON / OFF. 
 % Created Nov 5th 2025
+
+%% 1 - Create the table:
 T = combine_bar_results();
 
 T_all = T;
 
-% For the RNAi data:
-% "251017_all_results_table_RNAi_ttl.mat"
-polar_mean_by_strain(T)
-title("RNAi-ttl-control-ON", 'FontSize', 16)
+%% 2 - Polar plots:
+
+polar_mean_by_strain(T, "slow")
+title("MARCM-ttl-control-OFF-w+", 'FontSize', 16)
 f = gcf;
 f.Position = [620   510   620   457];
 
-% For the MARCM data:
-% "251017_all_results_table_MARCM_ttl.mat"
-polar_mean_by_genotype(T)
-title("MARCM-ttl-control-OFF", 'FontSize', 16)
-f = gcf;
-f.Position = [620   510   620   457];
+%% 3 - Box and whisker plots:
+
+columnName = "DSI_vector";
+plotGroupedBox(T, columnName)
+
+%% 4 - Stats
+
+results = runGroupedStats(T, columnName);
+
+
+ 
+
+
+%%
+
+% % For the RNAi data:
+% % "251017_all_results_table_RNAi_ttl.mat"
+% polar_mean_by_strain(T)
+% title("RNAi-ttl-control-ON", 'FontSize', 16)
+% f = gcf;
+% f.Position = [620   510   620   457];
+% 
+% % For the MARCM data:
+% % "251017_all_results_table_MARCM_ttl.mat"
+% polar_mean_by_genotype(T)
+% title("MARCM-ttl-control-OFF", 'FontSize', 16)
+% f = gcf;
+% f.Position = [620   510   620   457];
 
 
 %% For the MARCM data from Autumn 2025 onwards. "control" / "ttl" now added in the strain name. 
-
-polar_mean_by_strain(T, "slow")
-title("MARCM-ttl-control-ON", 'FontSize', 16)
-f = gcf;
-f.Position = [620   510   620   457];
-
+% 
+% polar_mean_by_strain(T, "slow")
+% title("MARCM-ttl-control-OFF-w+", 'FontSize', 16)
+% f = gcf;
+% f.Position = [620   510   620   457];
 
 % Convert from cell to double:
 
@@ -46,30 +69,30 @@ f.Position = [620   510   620   457];
 % % Now combine the tables
 % T_combined = [T; T_new];
 
-strainSel = "control";
-polar_mean_by_type_for_strain(T, strainSel)
-
-strainSel = "ttl";
-polar_mean_by_type_for_strain(T, strainSel)
+% strainSel = "control";
+% polar_mean_by_type_for_strain(T, strainSel)
+% 
+% strainSel = "ttl";
+% polar_mean_by_type_for_strain(T, strainSel)
 
 
 %% Different speeds
 
-
-polar_mean_by_strain(T, "slow")
-title("MARCM-ttl-control-OFF-28dps", 'FontSize', 16)
-f = gcf;
-f.Position = [620   510   620   457];
-
-polar_mean_by_strain(T, "fast")
-title("MARCM-ttl-control-OFF-56dps", 'FontSize', 16)
-f = gcf;
-f.Position = [620   510   620   457];
-
-polar_mean_by_strain(T, "vfast")
-title("MARCM-ttl-control-OFF-168dps", 'FontSize', 16)
-f = gcf;
-f.Position = [620   510   620   457];
+% 
+% polar_mean_by_strain(T, "slow")
+% title("MARCM-ttl-control-OFF-28dps", 'FontSize', 16)
+% f = gcf;
+% f.Position = [620   510   620   457];
+% 
+% polar_mean_by_strain(T, "fast")
+% title("MARCM-ttl-control-ON-56dps", 'FontSize', 16)
+% f = gcf;
+% f.Position = [620   510   620   457];
+% 
+% polar_mean_by_strain(T, "vfast")
+% title("MARCM-ttl-control-OFF-168dps", 'FontSize', 16)
+% f = gcf;
+% f.Position = [620   510   620   457];
 
 
 
